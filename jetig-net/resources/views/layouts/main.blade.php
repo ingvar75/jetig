5b2456791c3fd5c0fc5dcf3c2e4b0b26404f3cda
ingvar75 <ingvar0208@gmail.com>
2021-12-14 11:49:38 +0200

    add jetig lara

--- jetig-net/resources/views/layouts/main.blade.php
+++ jetig-net/resources/views/layouts/main.blade.php
@@ -49,8 +49,16 @@
                             </div>
                             <div class="jet-blockcontent">
                                 <div>
+                                    @if ($errors->any())
+                                        <div class="alert alert-danger">
+                                            <ul>
+                                                @foreach($errors->all() as $error)
+                                                    <li>{{ $error }}</li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    @endif
                                     @yield('content_login')
-
                                 </div>
                             </div>
                         </div>
