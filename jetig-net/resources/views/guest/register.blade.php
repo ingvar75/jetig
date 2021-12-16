3a592730f339a6e2920cf5c11ed49bcb9c845d53
ingvar75 <ingvar0208@gmail.com>
2021-12-16 12:07:17 +0200

    add jetig lara 16_12

--- jetig-net/resources/views/guest/register.blade.php
+++ jetig-net/resources/views/guest/register.blade.php
@@ -17,17 +17,46 @@
             <div class="jet-postcontent jet-postcontent-0 clearfix">
                 <div class="jet-content-layout">
                     <div class="jet-content-layout-row">
-                        <div class="jet-layout-cell layout-item-1" style="width: 100%" >
+                        <div class="jet-layout-cell layout-item-1" style="width: 100%">
                             <h3 style="border-bottom: 1px solid #776D50; padding-bottom: 5px">Welcome</h3>
-                            <div class="image-caption-wrapper" style="width: 60%; float: left"><img src="images/97067.jpg" style="width: 100%; max-width: 398px; " alt="an image" class="jet-lightbox"><p>Image by Flickr/mark roy</p></div><p><span style="font-weight: bold;">Libero mauris sodales nisi posuere consectetuer arcu imperdiet dui.</span></p><p>Ac ligula felis lacus ligula pellentesque nisl tortor. Magna aenean nisi metus tincidunt suspendisse class. Convallis elit non. Felis erat suscipit tortor ac aenean lobortis id praesent in. Tristique ipsum in. Vitae egestas odio posuere velit per bibendum blandit fusce nec sem. Praesent lorem a turpis quis tincidunt nisl. Ipsum sed non sapien ultrices et eu ac nisl ut. Vestibulum ullamcorper quis cras viverra gravida ut. Velit curabitur. Ac eu ligula nec dignissim suspendisse. In nulla vel ante donec ut nunc vestibulum commodo potenti etiam felis. A dictum vivamus erat etiam eros at vitae fusce augue.</p><p><a href="#" class="jet-button">Еще</a></p>
+                            <div class="image-caption-wrapper" style="width: 60%; float: left"><img
+                                    src="images/97067.jpg" style="width: 100%; max-width: 398px; " alt="an image"
+                                    class="jet-lightbox">
+                                <p>Image by Flickr/mark roy</p></div>
+                            <p><span style="font-weight: bold;">Libero mauris sodales nisi posuere consectetuer arcu imperdiet dui.</span>
+                            </p>
+                            <p>Ac ligula felis lacus ligula pellentesque nisl tortor. Magna aenean nisi metus tincidunt
+                                suspendisse class. Convallis elit non. Felis erat suscipit tortor ac aenean lobortis id
+                                praesent in. Tristique ipsum in. Vitae egestas odio posuere velit per bibendum blandit
+                                fusce nec sem. Praesent lorem a turpis quis tincidunt nisl. Ipsum sed non sapien
+                                ultrices et eu ac nisl ut. Vestibulum ullamcorper quis cras viverra gravida ut. Velit
+                                curabitur. Ac eu ligula nec dignissim suspendisse. In nulla vel ante donec ut nunc
+                                vestibulum commodo potenti etiam felis. A dictum vivamus erat etiam eros at vitae fusce
+                                augue.</p>
+                            <p><a href="#" class="jet-button">Еще</a></p>
                         </div>
                     </div>
                 </div>
                 <div class="jet-content-layout-br layout-item-0">
-                </div><div class="jet-content-layout">
+                </div>
+                <div class="jet-content-layout">
                     <div class="jet-content-layout-row">
-                        <div class="jet-layout-cell layout-item-1" style="width: 100%" >
-                            <div class="image-caption-wrapper" style="width: 60%; float: left"><img src="images/1bcdf.jpg" style="width: 100%; max-width: 399px;" alt="an image" class="jet-lightbox"><p>Image by Flickr/Richard Taylor</p></div><p><span style="font-weight: bold;">Nullam quis tempus libero justo eleifend nunc vel ut cursus neque.</span></p><p>cursus conubia ipsum eget nec in mi sem sem felis nec. Molestie aliquet imperdiet in leo feugiat nunc metus. Nec metus ultricies. Condimentum ut ad volutpat sit odio in nunc blandit ut nibh. Cubilia magna id. Orci ultrices in hendrerit aliquam malesuada proin mi nullam luctus. Nam urna curae nunc eleifend faucibus turpis. In at nam ut consectetuer vestibulum vivamus nec. Elit quisque eu leo eget dictum ligula euismod pede. Nisl aenean diam. Vestibulum magna blandit pretium etiam. At amet ante ac vitae orci fusce velit pellentesque vel orci tortor id. Pellentesque iaculis amet rhoncus sed vehicula magna ut.</p><p><a href="#">Еще</a></p>
+                        <div class="jet-layout-cell layout-item-1" style="width: 100%">
+                            <div class="image-caption-wrapper" style="width: 60%; float: left"><img
+                                    src="images/1bcdf.jpg" style="width: 100%; max-width: 399px;" alt="an image"
+                                    class="jet-lightbox">
+                                <p>Image by Flickr/Richard Taylor</p></div>
+                            <p><span style="font-weight: bold;">Nullam quis tempus libero justo eleifend nunc vel ut cursus neque.</span>
+                            </p>
+                            <p>cursus conubia ipsum eget nec in mi sem sem felis nec. Molestie aliquet imperdiet in leo
+                                feugiat nunc metus. Nec metus ultricies. Condimentum ut ad volutpat sit odio in nunc
+                                blandit ut nibh. Cubilia magna id. Orci ultrices in hendrerit aliquam malesuada proin mi
+                                nullam luctus. Nam urna curae nunc eleifend faucibus turpis. In at nam ut consectetuer
+                                vestibulum vivamus nec. Elit quisque eu leo eget dictum ligula euismod pede. Nisl aenean
+                                diam. Vestibulum magna blandit pretium etiam. At amet ante ac vitae orci fusce velit
+                                pellentesque vel orci tortor id. Pellentesque iaculis amet rhoncus sed vehicula magna
+                                ut.</p>
+                            <p><a href="#">Еще</a></p>
                         </div>
                     </div>
                 </div>
@@ -56,23 +85,23 @@
                         </div>
                         <br>
                     @endif
-    {{ Form::open(['url' => 'register', 'method' => 'post']) }}
+                    {{ Form::open(['url' => 'register', 'method' => 'post']) }}
 
-    {{ Form::label('name', 'Name') }}
-    {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Name']) }}
+                    {{ Form::label('name', 'Name') }}
+                    {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Name']) }}
 
-    {{ Form::label('email', 'Email') }}
-    {{ Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Email']) }}
+                    {{ Form::label('email', 'Email') }}
+                    {{ Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Email']) }}
 
-    {{ Form::label('password', 'Password') }}
-    {{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) }}
+                    {{ Form::label('password', 'Password') }}
+                    {{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) }}
 
-    {{ Form::label('repeatPassword', 'Repeat Password') }}
-    {{ Form::password('repeatPassword', ['class' => 'form-control', 'placeholder' => 'Repeat Password']) }}
+                    {{ Form::label('repeatPassword', 'Repeat Password') }}
+                    {{ Form::password('repeatPassword', ['class' => 'form-control', 'placeholder' => 'Repeat Password']) }}
 
-                        <p><br> {{ Form::submit('Реєстрація', ['class' => 'jet-button']) }}</p>
+                    <p><br> {{ Form::submit('Реєстрація', ['class' => 'jet-button']) }}</p>
 
-    {{ Form::close() }}
+                    {{ Form::close() }}
 
-    <p><a href="/login">Вхід</a></p>
+                    <p><a href="/login">Вхід</a></p>
 @stop
--- jetig-net/resources/views/layouts/main.blade.php
+++ jetig-net/resources/views/layouts/main.blade.php
@@ -2,12 +2,14 @@
 <html dir="ltr" lang="en-US">
 <head><!-- Created by IG-->
     <meta charset="utf-8">
-    <title>New Page</title>
+    <title>JetIG - Товари для активного відпочинку</title>
     <meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0, user-scalable = no, width = device-width">
 
-    <!--[if lt IE 9]><script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!--[if lt IE 9]>
+    <script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     <link rel="stylesheet" href="/css/style.css" media="screen">
-    <!--[if lte IE 7]><link rel="stylesheet" href="/css/style.ie7.css" media="screen" /><![endif]-->
+    <!--[if lte IE 7]>
+    <link rel="stylesheet" href="/css/style.ie7.css" media="screen"/><![endif]-->
     <link rel="stylesheet" href="/css/style.responsive.css" media="all">
     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu&amp;subset=latin">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
@@ -16,12 +18,31 @@
     <script src="/js/script.responsive.js"></script>
     <meta name="description" content="Description">
     <meta name="keywords" content="Keywords">
-    <style>.jet-content .jet-postcontent-0 .layout-item-0 { border-top-width:1px;border-top-style:solid;border-top-color:#776D50;margin-top: 5px;margin-bottom: 5px;  }
-        .jet-content .jet-postcontent-0 .layout-item-1 { padding-right: 10px;padding-left: 10px;  }
-        .ie7 .jet-post .jet-layout-cell {border:none !important; padding:0 !important; }
-        .ie6 .jet-post .jet-layout-cell {border:none !important; padding:0 !important; }
+    <style>.jet-content .jet-postcontent-0 .layout-item-0 {
+            border-top-width: 1px;
+            border-top-style: solid;
+            border-top-color: #776D50;
+            margin-top: 5px;
+            margin-bottom: 5px;
+        }
 
-    </style></head>
+        .jet-content .jet-postcontent-0 .layout-item-1 {
+            padding-right: 10px;
+            padding-left: 10px;
+        }
+
+        .ie7 .jet-post .jet-layout-cell {
+            border: none !important;
+            padding: 0 !important;
+        }
+
+        .ie6 .jet-post .jet-layout-cell {
+            border: none !important;
+            padding: 0 !important;
+        }
+
+    </style>
+</head>
 <body>
 <div id="jet-main">
     <header class="jet-header">
@@ -43,71 +64,89 @@
                     @yield('content')
 
 
-                                    @yield('content_login')
-                                </div>
-                            </div>
-                        </div>
-                        <div class="jet-block clearfix">
-                            <div class="jet-blockheader">
-                                <h3 class="t">Пошук</h3>
-                            </div>
-                            <div class="jet-blockcontent"><div>
-                                    <form action="#" class="jet-search" method="get" name="searchform">
-                                        <input type="text" value="" name="s" />
-                                        <input type="submit" value="Search" name="search" class="jet-search-button" />
-                                    </form>
-                                </div></div>
-                        </div>
-                        <div class="jet-block clearfix">
-                            <div class="jet-blockheader">
-                                <h3 class="t">Категорії</h3>
-                            </div>
-                            <div class="jet-blockcontent"><div>
-                                    <p>Lorem ipsum dolor sit amet. Nam sit amet sem. Mauris a ante.</p>
-                                    <ul>
-                                        <li>
-                                            <a href="#" title="All News">All News</a> (50)
-                                        </li>
-                                        <li>
-                                            <a href="#" title="Best of the Year">Best of the Year</a> (4)
-                                        </li>
-                                        <li>
-                                            <a href="#" title="Hyperlink">Hyperlink</a> (24)
-                                        </li>
-                                        <li>
-                                            <a href="#" title="Visited link" class="visited">Visited link</a> (17)
-                                        </li>
-                                        <li>
-                                            <a href="#" title="Hovered link" class="hover">Hovered link</a> (6)
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="jet-block clearfix">
-                            <div class="jet-blockheader">
-                                <h3 class="t">TODAY's Best</h3>
-                            </div>
-                            <div class="jet-blockcontent"><p><img width="184" height="210" alt="" src="images/blockimage4.jpg" class=""></p><p style="text-align: center;"><a href="#">Read more »</a></p></div>
-                        </div><div class="jet-block clearfix">
-                            <div class="jet-blockheader">
-                                <h3 class="t">Hottest Trends</h3>
-                            </div>
-                            <div class="jet-blockcontent"><p style="text-align:center;"><img width="181" height="181" alt="" src="images/blockimage1.jpg" class=""></p><p style="text-align: center;"><a href="#">Read more »</a></p></div>
-                        </div>
-                        <div class="jet-block clearfix">
-                            <div class="jet-blockheader">
-                                <h3 class="t">Поділитись</h3>
-                            </div>
-                            <div class="jet-blockcontent"><p><img width="51" height="51" alt="" src="images/1308560868_rss.png" class=""><img width="51" height="51" alt="" src="images/1308647898_facebook-2.png" class=""><img width="51" height="51" alt="" src="images/1308560877_flickr-2.png" class=""></p></div>
-                        </div>
-                    </div>
+                    @yield('content_login')
                 </div>
             </div>
-        </div><footer class="jet-footer">
-            <div style="position:relative;text-align:center;"><a title="RSS" class="jet-rss-tag-icon" style="position: absolute; bottom: -10px; left: -6px; line-height: 32px;" href="#"></a><p><a href="#">Новини</a>|<a href="#">Галерея</a>|<a href="#">Контакти</a>|<a href="http://www.iconfinder.com/search/?q=iconset:web2badges">Icon set</a> A. <a href="#">IG</a></p><p>Авторське право © 2021. Всі права захищені.</p></div>
-        </footer>
-
+        </div>
+        <div class="jet-block clearfix">
+            <div class="jet-blockheader">
+                <h3 class="t">Пошук</h3>
+            </div>
+            <div class="jet-blockcontent">
+                <div>
+                    <form action="#" class="jet-search" method="get" name="searchform">
+                        <input type="text" value="" name="s"/>
+                        <input type="submit" value="Search" name="search" class="jet-search-button"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="jet-block clearfix">
+            <div class="jet-blockheader">
+                <h3 class="t">Категорії</h3>
+            </div>
+            <div class="jet-blockcontent">
+                <div>
+                    <p>Lorem ipsum dolor sit amet. Nam sit amet sem. Mauris a ante.</p>
+                    <ul>
+                        <li>
+                            <a href="#" title="All News">All News</a> (50)
+                        </li>
+                        <li>
+                            <a href="#" title="Best of the Year">Best of the Year</a> (4)
+                        </li>
+                        <li>
+                            <a href="#" title="Hyperlink">Hyperlink</a> (24)
+                        </li>
+                        <li>
+                            <a href="#" title="Visited link" class="visited">Visited link</a> (17)
+                        </li>
+                        <li>
+                            <a href="#" title="Hovered link" class="hover">Hovered link</a> (6)
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="jet-block clearfix">
+            <div class="jet-blockheader">
+                <h3 class="t">TODAY's Best</h3>
+            </div>
+            <div class="jet-blockcontent"><p><img width="184" height="210" alt="" src="images/blockimage4.jpg" class="">
+                </p>
+                <p style="text-align: center;"><a href="#">Read more »</a></p></div>
+        </div>
+        <div class="jet-block clearfix">
+            <div class="jet-blockheader">
+                <h3 class="t">Hottest Trends</h3>
+            </div>
+            <div class="jet-blockcontent"><p style="text-align:center;"><img width="181" height="181" alt=""
+                                                                             src="images/blockimage1.jpg" class=""></p>
+                <p style="text-align: center;"><a href="#">Read more »</a></p></div>
+        </div>
+        <div class="jet-block clearfix">
+            <div class="jet-blockheader">
+                <h3 class="t">Поділитись</h3>
+            </div>
+            <div class="jet-blockcontent"><p><img width="51" height="51" alt="" src="images/1308560868_rss.png"
+                                                  class=""><img width="51" height="51" alt=""
+                                                                src="images/1308647898_facebook-2.png" class=""><img
+                        width="51" height="51" alt="" src="images/1308560877_flickr-2.png" class=""></p></div>
+        </div>
     </div>
 </div>
-</body></html>
+</div>
+</div>
+<footer class="jet-footer">
+    <div style="position:relative;text-align:center;"><a title="RSS" class="jet-rss-tag-icon"
+                                                         style="position: absolute; bottom: -10px; left: -6px; line-height: 32px;"
+                                                         href="#"></a>
+        <p><a href="#">Новини</a>|<a href="#">Галерея</a>|<a href="#">Контакти</a>|<a
+                href="http://www.iconfinder.com/search/?q=iconset:web2badges">Icon set</a> A. <a href="#">IG</a></p>
+        <p>Авторське право © 2021. Всі права захищені.</p></div>
+</footer>
+
+</div>
+</div>
+</body>
+</html>
