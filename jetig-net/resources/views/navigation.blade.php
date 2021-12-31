27d394fccf423fd19971e7d75047bb829c505ab9
ingvar75 <ingvar0208@gmail.com>
2021-12-31 09:06:44 +0200

    add jetig lara 31_12

--- jetig-net/resources/views/navigation.blade.php
+++ jetig-net/resources/views/navigation.blade.php
@@ -1,14 +1,32 @@
 @extends('layouts.main')
 <?php
 
+namespace App\Http\Controllers;
+class NavActive
+{
+    public function navigation()
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
+}
+
+$nav = new NavActive;
+$action = $nav->navigation();
+
 ?>
 @section('nav')
-<nav class="jet-nav">
-    <ul class="jet-hmenu">
-        <li><a href="{{route('home')}}" class="active">Домівка</a></li>
-        <!--<li><a href="new-page-2-2.html">Категорії товарів</a></li>-->
-        <li><a href="{{route('contacts')}}">Контакти</a></li>
-        <!--<li><a href="кошик.html">Кошик</a></li>-->
-    </ul>
-</nav>
+    <nav class="jet-nav">
+        <ul class="jet-hmenu">
+            <?php if($action == '/' || $action == '/excel/view'){?>
+            <li><a href="{{route('home')}}" class="active">Домівка</a></li>
+            <?php }else{?>
+            <li><a href="{{route('home')}}">Домівка</a></li><?php }?>
+
+            <?php if($action == '/contacts') {?>
+            <li><a href="{{route('contacts')}}" class="active">Контакти</a></li>
+            <?php }else{?>
+            <li><a href="{{route('contacts')}}">Контакти</a></li><?php }?>
+
+        </ul>
+    </nav>
 @stop
