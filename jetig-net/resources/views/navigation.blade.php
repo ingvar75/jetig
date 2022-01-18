004e4dae94091233d69e1ab8b008791e02f7f61b
ingvar75 <ingvar0208@gmail.com>
2022-01-18 16:31:47 +0200

    Changes 18_01_2022

--- jetig-net/resources/views/navigation.blade.php
+++ jetig-net/resources/views/navigation.blade.php
@@ -2,7 +2,7 @@
 <?php
 
 namespace App\Http\Controllers;
-class NavActive
+use Illuminate\Support\Facades\DB;class NavActive
 {
     public function navigation()
     {
@@ -17,25 +17,39 @@ $action = $nav->navigation();
 @section('nav')
     <nav class="jet-nav">
         <ul class="jet-hmenu">
-            <?php if($action == '/' || $action == '/excel/view' || $action == '/login' || $action == '/register'){?>
+            <?php
+            $CatParent = DB::table('categories')->where('id_group_parent', 0)->get();
+
+            //var_dump($CatParent);exit;
+            if($action == '/' || $action == '/excel/view' || $action == '/login' || $action == '/register'){?>
             <li><a href="{{route('home')}}" class="active">Домівка</a></li>
             <?php }else{?>
             <li><a href="{{route('home')}}">Домівка</a></li><?php }?>
 
-            <?php if($action == 'categories'){?>
+            <?php
+                if($action == 'categories'){?>
             <li><a href="{{route('home')}}" class="active">Категорії</a>
                 <ul class="active">
-                    <li><a href="авторизація/вхід.html">Вхід</a></li>
-                    <li><a href="авторизація/реєстрація.html">Реєстрація</a></li>
+                    <?php
+                    if (count($CatParent) > 0){
+                    for ($i=0; $i<count($CatParent); $i++){?>
+                    <li><a href="авторизація/вхід.html"><?=$CatParent[$i]->name_group?></a></li>
+                    <?php
+                    }}?>
                 </ul>
             </li>
             <?php }else{?>
             <li><a href="{{route('home')}}" class="link">Категорії</a>
                 <ul>
-                    <li><a href="авторизація/вхід.html">Вхід</a></li>
-                    <li><a href="авторизація/реєстрація.html">Реєстрація</a></li>
+                    <?php
+                    if (count($CatParent) > 0){
+                    for ($i=0; $i<count($CatParent); $i++){?>
+                    <li><a href="авторизація/вхід.html"><?=$CatParent[$i]->name_group?></a></li>
+                    <?php
+                    }}?>
                 </ul>
-            </li><?php }?>
+            </li><?php
+                }?>
 
             <?php if($action == '/contacts') {?>
             <li><a href="{{route('contacts')}}" class="active">Контакти</a></li>
