168fc236548dd1bf75bda449c2caca280fc5cd2d
ingvar75 <ingvar0208@gmail.com>
2022-02-08 11:20:55 +0200

    Changes 08_02_2022

---
+++ jetig-net/database/migrations/2022_02_08_084909_create_basket_user_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBasketUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('basket_user', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('user_email');
+            $table->string('mob_tel');
+            $table->string('ses_token');
+            $table->string('xsrf_token');
+            $table->string('b_status');
+            $table->string('product_code');
+            $table->integer('id_group');
+            $table->integer('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('basket_user');
+    }
+}
