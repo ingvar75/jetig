9b8c26f7d649ca5bb70c3f1b60fd8e5c876824fa
ingvar75 <ingvar0208@gmail.com>
2022-02-16 09:20:45 +0200

    Changes 16_02_2022

--- jetig-net/resources/views/contacts.blade.php
+++ jetig-net/resources/views/contacts.blade.php
@@ -1,13 +1,15 @@
 @extends('navigation')
 <?php
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 $user = Auth::user();
 $sess = Session::all();
+
 ?>
-@csrf
 @section('content')
+
     <div class="jet-layout-cell jet-content">
         <article class="jet-post jet-article">
 
@@ -16,27 +18,60 @@ $sess = Session::all();
                     <div class="jet-content-layout-row">
                         <div class="jet-layout-cell layout-item-1" style="width: 100%">
                             <h3 style="border-bottom: 1px solid #776D50; padding-bottom: 5px">Контакти</h3>
+                            <?php
+                            $sent = DB::table('mail')->where('ses_token', $sess['_token'])->doesntExist();
+                            if ($sent == true && isset($_POST['email']) && isset($_POST['comment'])) {
+                                if (!isset($user)) {
+                                    $user_id = 'notreg';
+                                } else {
+                                    $user_id = $user['id'];
+                                }
+                                DB::table('mail')->insertOrIgnore(['user_id' => $user_id,
+                                    'user_email' => $_POST['email'],
+                                    'ses_token' => $sess['_token'],
+                                    'created_at' => date("Y-m-d H:i:s"),
+                                    'updated_at' => date("Y-m-d H:i:s"),])
+
+                                ?>
+                            <p>
+                                <span style="font-size:16px; font-weight: bold; color: #16be0a;">
+                                    Повідомлення успішно відправлено!
+                                </span>
+                            </p>
+                            <?php
+                                }elseif($sent == false && isset($_POST['email']) && isset($_POST['comment'])) {
+                                ?>
+                            <p>
+                                <span style="font-size:16px; font-weight: bold; color: #a7031b;">
+                                    Ліміт листування вичерпано! Спробуйте пізніше.
+                                </span>
+                            </p>
+                            <?php
+                            }
+                            ?>
                             <p><span style="font-weight: bold; color: #0a53be;">Пн-Пт 10:00-18:00:</span></p>
                             <p>+380679185706</p>
                             <p>+380733059806</p>
                             <p><span style="font-weight: bold; color: #0a53be">Email:</span></p>
                             <p>mail@jetig.net</p>
-                            <form action="" method="post">
-                            <?php
-                                if (isset($user)) {
-                                    ?>
-                                <p>
-                                    <label>
-                                        <input type="email" style="width: 25%;" name="email" value="<?=$user['email']?>">
-                                    </label>
-                                <p><input type="hidden" name="key" value="<?=$sess['_token']?>"></p>
-                                </p>
+                            <form name="send_mail" action="" method="post" onsubmit="return VerifySendForm();">
                                 <?php
-                                }else {
+                                if (isset($user)) {
                                 ?>
                                 <p>
                                     <label>
-                                        <input type="email" style="width: 25%;" placeholder="Ваш Email">
+                                        <input type="email" style="width: 25%;" name="email" value="<?=$user['email']?>"
+                                               readonly>
+                                    </label>
+                                </p>
+                                <p><input type="hidden" name="key" value="<?=$sess['_token']?>"></p>
+                                <?php
+                                }else{
+                                ?>
+                                <p>
+                                    <label>
+                                        <input type="email" style="width: 25%;" name="email" value=""
+                                               placeholder="Ваш Email">
                                     </label>
                                 </p>
                                 <p><input type="hidden" name="key" value="<?=$sess['_token']?>"></p>
@@ -52,6 +87,22 @@ $sess = Session::all();
                                 @csrf
                                 <p><input type="submit" class="jet-button" value="Відправити повідомлення"></p>
                             </form>
+                            <script>
+                                function VerifySendForm() {
+                                    if (document.send_mail.email.value == "") {
+                                        alert("Поле Email не може бути порожнім.");
+                                        document.send_mail.email.focus();
+                                        return false;
+                                    } else if (document.send_mail.comment.value == "") {
+                                        alert("Повідомлення не може бути порожнім.");
+                                        document.send_mail.comment.focus();
+                                        return false;
+                                    } else
+                                        document.form.submit();
+                                    return true;
+                                }
+
+                            </script>
                         </div>
                     </div>
                 </div>
