87b8827c8aac0de4f3ffc1c4a8dca7bfb91d64d1
ingvar75 <ingvar0208@gmail.com>
2022-02-22 16:29:37 +0200

    Changes 21_02_2022

--- jetig-net/resources/views/translator.blade.php
+++ jetig-net/resources/views/translator.blade.php
@@ -3,20 +3,29 @@
 use Illuminate\Support\Facades\DB;
 
 if (isset($_GET['choose']) && $_GET['choose'] == 'category') {
+    $fh = fopen("jetCat.txt", 'w') or die("Failed to create file");
     $dataCat = DB::table('categories')->select('id', 'name_group')->get();
     foreach ($dataCat as $item) {
         echo strip_tags(($item->id))."<br>";
         echo strip_tags(($item->name_group))."<br>";
+        fwrite($fh, strip_tags(($item->id))."\r\n") or die("Could not write to file");
+        fwrite($fh, strip_tags(($item->name_group))."\r\n") or die("Could not write to file");
     }
+    fclose($fh);
     exit;
 }
 if (isset($_GET['choose']) && $_GET['choose'] == 'products') {
+    $fh = fopen("jetProd.txt", 'w') or die("Failed to create file");
     $dataProd = DB::table('products')->select('id', 'item_name', 'description')->get();
     foreach ($dataProd as $item) {
         echo strip_tags(($item->id))."<br>";
         echo strip_tags(($item->item_name))."<br>";
         echo strip_tags(($item->description))."<br>";
+        fwrite($fh, strip_tags(($item->id))."\r\n") or die("Could not write to file");
+        fwrite($fh, strip_tags(htmlentities($item->item_name))."\r\n") or die("Could not write to file");
+        fwrite($fh, strip_tags(htmlentities($item->description))."\r\n") or die("Could not write to file");
     }
+    fclose($fh);
 exit;
 }
 ?>
